dea9564520774e0f590db31241e9fc31c6d99fe7
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-03-10 22:22:40 +0530

    basics updated

--- Tutorials/basics.dart
+++ Tutorials/basics.dart
@@ -2,9 +2,16 @@ void main(){
   var name = "abc";
   print('${name}'); //print(name)  //print("name:" + name) //print('$name')
 
+  //final and const 
   final age = 20; //this becomes a runtime constant
   print(age);
   const isOpen = true; //this becomes a compile time constant
   print(isOpen);
+
+  //data types
+  //int, double, bool, String
+
+  int? points; //this question mark will give the vlaue null if "points" remains unassigned.
+  print(points);
 
 }
